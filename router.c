bc40d6fe057d0f886894eab3df65ffae62e00947
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-01 16:53:29 -0300

    Commit inicial com router.c :tada:

---
+++ router.c
@@ -0,0 +1,22 @@
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+ 
+#define SERVER "127.0.0.1"
+#define BUFLEN 512  //Max length of buffer
+#define PORT 8888   //The port on which to send data
+
+typedef struct
+{
+    int id, port, msg_in, msg_out;
+    char ip[32];
+}router;
+
+
+int main() {
+  int tab_rot[N_ROT][N_ROT];	  int tab_rot[N_ROT][N_ROT];
+  
+  return 0;
+}
