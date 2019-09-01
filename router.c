b3b6cb32dfa60030d4ade814c6236b2ab2acdaa0
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-01 17:23:30 -0300

    :construction: Adicionando structs

--- router.c
+++ router.c
@@ -1,22 +1,34 @@
-#include<stdio.h> //printf
-#include<string.h> //memset
-#include<stdlib.h> //exit(0);
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
+#include<pthread.h>
  
 #define SERVER "127.0.0.1"
-#define BUFLEN 512  //Max length of buffer
-#define PORT 8888   //The port on which to send data
+#define BUFLEN 512 
+#define PORT 8888
+#define QUEUE_SIZE 92
+#define MSG_SIZE 100
+#define NUM_ROT 8  
 
-typedef struct
-{
+typedef struct{
     int id, port, msg_in, msg_out;
     char ip[32];
 }router;
 
+typedef struct{
+    int origin, dest, type;
+    char content[MSG_SIZE];
+
+}package;
+
+typedef struct{
+    int origin, dest, cost, adj[NUM_ROT]
+}table;
 
 int main() {
-  int tab_rot[N_ROT][N_ROT];	  int tab_rot[N_ROT][N_ROT];
+  int tab_rot[NUM_ROT][NUM_ROT];
   
   return 0;
 }
