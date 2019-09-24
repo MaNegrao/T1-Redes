ab8ed6ebbe5b114a432666c79b3b45ef12cc510d
isabelirk <isabelireik2@gmail.com>
2019-09-23 21:56:43 -0300

    :construction: inicialização das configurações

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <pthread.h>
+
+typedef struct setar_configuracoes{
+	int s;
+}setar_configuracoes;
+
+setar_configuracoes inicializa_protocolo(int ID){
+	setar_configuracoes set_config;
+	set_config.s = ID;
+	return set_config;
+}
 
 int main(void){
 	int ID;
+	setar_configuracoes set_config;
 
-	printf("Digite o identificador do roteador instanciado: ");
-	scanf("%d", ID);	
+	printf("Digite o identificador do roteador instanciado: \n");
+	scanf("%d", &ID);	
+	set_config = inicializa_protocolo(ID);
 
 	return 0;
 }
