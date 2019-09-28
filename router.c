372fc6215f5a20871ba8c2d996dd3b6929579074
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-28 18:55:23 -0300

    :construction: updating structs and starting router

--- router.c
+++ router.c
@@ -1,23 +1,20 @@
 #include "router.h"
 
 pthread_t receiver_thread, sender_thread;
-int id;
+int id_router;
 
-//função que retorna os erros que aconteçam na execução e a encerra
-void die(char *s){
+void die(char *s){ //função que retorna os erros que aconteçam na execução e a encerra
 	perror(s);
 	exit(1);
 }
 
-//função auxiliar para as configurações iniciais
-set_config inicializa_protocolo(int id){
+/*set_config inicializa_protocolo(){ //função auxiliar para as configurações iniciais
 	set_config config;
-	config.id = id;
+	config.id = id_router;
 	return config;
-}
+}*/
 
-//apenas uma função de menu
-void menu(int id_router){
+void menu(){ //apenas uma função de menu
 		system("clear");
 		printf("\t\t┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
 		printf("\t\t┃            Router 0%d            ┃\n", id_router);
@@ -30,22 +27,22 @@ void menu(int id_router){
 		printf("\t\t┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n\n\t\t  ");
 }
 
-void *receiver(void *data){
-
-}
-
-//função da thread sender
-void *sender(void *data){
+void *sender(void *data){ //função da thread sender
 	char buf[BUFLEN];
 	int id_destiny, op;
 
 	while(1){
-		menu(id);
+		menu();
 		scanf("%d", &op);
-
 		switch(op){
+			case 0:
+				exit(0);
+				break;
 			case 1:
 				printf("joia\n");
+				sleep(1);
+				break;
+			case 2:
 				break;
 			default:
 				printf("Opção inválida!\n");
@@ -54,23 +51,29 @@ void *sender(void *data){
 	}
 }
 
+void *receiver(void *data){
+
+}
+
 int main(int argc, char *argv[]){
-	set_config config;
+	int router_table[N_ROT][N_ROT];
 
 	if(argc < 2)
 		die("Insira o ID do roteador!\n");
 	else if(argc > 2)		
 		die("Insira apenas um ID para o roteador!\n");
+	id_router = strtol(argv[1], NULL, 10); //função de casting do argv id para int 
+
+	if(id_router >= N_ROT){
+		die("ID de roteador inválido!\n");
+	}
+
+	memset(router_table, -1, sizeof(int) * N_ROT * N_ROT);
 
 	pthread_create(&receiver_thread, NULL, receiver, NULL);
 	pthread_create(&sender_thread, NULL, sender, NULL);
 
 	pthread_join(sender_thread, NULL);
-
-
-	id = strtol(argv[1], NULL, 10); //função de casting do argv id para int 
-
-	//config = inicializa_protocolo(id);
 
 	return 0;
 }
--- router.h
+++ router.h
@@ -1,34 +1,24 @@
 #include<stdio.h>
+#include<unistd.h>
 #include<string.h>
 #include<stdlib.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
 #include<pthread.h>
 
-#define SERVER "127.0.0.1"
 #define BUFLEN 512  //Max length of buffer
+#define N_ROT 6
+#define MSG_SIZE 100
 
-typedef struct set_config{
-	int id;
-}set_config;
+typedef struct router{
+     int id, port, msg_in, msg_out;
+     char ip[32];
+}router;
 
-//struct dos vertices para o grafo da rede
-typedef struct nodo{
-	int id;
-	int custo;
-	struct nodo *next;
-}vertex;
-
-// typedef struct{
-//     int id, port, msg_in, msg_out;
-//     char ip[32];
-// }router;
-
-// typedef struct{
-//     int origin, dest, type;
-//     char content[MSG_SIZE];
-
-// }package;
+typedef struct{
+    int origin, dest, type;
+    char content[MSG_SIZE];
+}package;
 
 // typedef struct{
 //     int origin, dest, cost, path[NUM_ROT];
